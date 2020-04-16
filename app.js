786095708f6462037ebd6cc3b29ecaacd7ad6085
jpablo263 <jpablo263@gmail.com>
2020-04-15 22:10:35 -0300

    implement routing for contact and about pages

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 var http = require('http');
 
 http.createServer(function(req, res) {
-    res.write("Hello world");
+    res.writeHead(200, {'content-type': 'text/html'});
+
+    var url = req.url;
+
+    if(url === '/contact'){
+        res.write("<h1>Contacto</h1>");
+        res.end();
+    } else if(url === '/about') {
+        res.write("<h1>Acerca de</h1>");
+        res.end();
+    } else {
+        res.write("<h1>Bienvenido</h1>");
+        res.end();
+    }
+
+    
     res.end();
 }).listen(3000,function(){
     console.log("servidor escuchando en puerto 3000")
